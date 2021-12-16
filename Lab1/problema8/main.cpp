d555996b08524177390107880b64468c537d50ae
juan_giraldo <juanjogiral02@gmail.com>
2021-12-16 17:00:25 -0500

    problema 8

--- Lab1/problema8/main.cpp
+++ Lab1/problema8/main.cpp
@@ -1,30 +1,30 @@
+// no se porque no me da la segunda parte
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int a,a1,b,b1,c,i,i2;
+    int a,b,c,i,i2;
+    double a1,a2;//b1,b2;
 
     cout << "Escriba los 3 numeros: ";
-    cin>>a>>c;
+    cin>>a/*>>b*/>>c;
 
-    for(i=1;i<=c;i = i*a){
-        a = a*i;
-        a1 = a1+a;
-        cout<<a<<"si"<<endl;
-
-
-        /*for(i2=1;i2<=c;i2 = i2 * b){
-            b = b*i;
-            b1 = b1+b;
-            cout<<b<<"sis"<<endl;
-        }*/
-
-
+    for(i=i;a1<c;i=i+1){
+        a1 = a*i;
+        a2 = a1+a2;
+        cout<<a1<<"si"<<endl;
     }
-    cout<<a1<<endl;
 
+    /*for(i2=i2;b1<c;i2=i2+1){
+        b1 = b*i2;
+        b2 = b1+b2;
+        cout<<b1<<"non"<<endl;
+    }*/
 
+    cout<<"suma: "<<a2<<endl;
+    //cout<<"\nSuma 1 "<<b2;
 
     return 0;
 }
