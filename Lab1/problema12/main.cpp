753743b52dd64cc74f82ea169b99783c41813dc1
juan_giraldo <juanjogiral02@gmail.com>
2021-12-16 22:29:29 -0500

    Termino el 12

--- Lab1/problema12/main.cpp
+++ Lab1/problema12/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(){
+    int num;
+    cout<<"Ingrese el numero: ";cin>>num;
+
+    for(int i=2;num>1;i++){
+        while (num%i == 0){
+            num /=i;
+            if(num == 1){
+              cout<<endl<<i;
+            }
+        }
+    }
+
+
     return 0;
 }
